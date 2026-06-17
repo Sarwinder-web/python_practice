56f8cc7e38dd187f4a1797e42130cb715624976c
sarwinder - web <manve192930@gmail.com>
2026-06-17 18:24:46 +0530

    Add combination sum using backtracking

--- combination_sum.py
+++ combination_sum.py
@@ -1,1 +1,25 @@
-print("hello bruder")
+def combination_sum(nums,target,index,current):
+
+    if sum(current)==target:
+        print(current)
+        return 
+    
+    if sum(current)>target:
+        return
+    
+    if index==len(nums):
+        return
+    
+    current.append(nums[index])
+
+    combination_sum(nums,target,index,current)
+
+    current.pop()
+
+    combination_sum(nums,target,index+1,current)
+
+nums = [5,6,9]
+target = 56
+
+combination_sum(nums,target,0,[])
+
