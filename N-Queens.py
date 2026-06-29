90a5afe6d75777c029fb13c142059018e514cc7b
sarwinder - web <manve192930@gmail.com>
2026-06-29 14:09:13 +0530

    Add new backtrack problem

--- N-Queens.py
+++ N-Queens.py
@@ -1,52 +1,38 @@
-def isSafe(board, row, col):
-    n = len(board)
+def is_ssafe(board,row,col,n):
 
-    # column check
-    for i in range(row - 1, -1, -1):
-        if board[i][col] == 1:
+    for i in range(row):
+        if board[i][col] == "Q":
             return False
-
-    # left diagonal ↖
-    i, j = row - 1, col - 1
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
+    
+    
+    i , j = row , col 
+    while i>=0 and j>=0:
+        if board[i][j] == "Q":
             return False
-        i -= 1
-        j -= 1
-
-    # right diagonal ↗
-    i, j = row - 1, col + 1
-    while i >= 0 and j < n:
-        if board[i][j] == 1:
+        i -=1
+        j -=1
+    
+    i , j = row , col
+    while i>=0 and j<n:
+        if board[i][j] == "Q":
             return False
         i -= 1
         j += 1
 
     return True
-
-def solve(board, row):
-    n = len(board)
-
-    # base case
-    if row == n:
-        for r in board:
-            print(r)
-        print()
+    
+def solve(board,row,n):
+    if row==n:
+        print(board)
         return
-
-    # try every column in this row
+        
     for col in range(n):
+        if is_ssafe(board,row,col,n):
+            board[row][col]="Q"
+            solve(board,row +1 ,n)
+            board[row][col]="."
 
-        if isSafe(board, row, col):
-
-            board[row][col] = 1      # place queen
-
-            solve(board, row + 1)    # go to next row
-
-            board[row][col] = 0      # backtrack
-
-# driver code
 n = 4
-board = [[0 for _ in range(n)] for _ in range(n)]
+board = [["." for _ in range(n)] for _ in range(n)]
 
-solve(board, 0)
+
